4da748e7465be9a39a178fde6132d25ccb4da186
vootlavenky63 <vootla.venky63@gmail.com>
2019-09-29 17:13:33 +0530

    Crud operation

--- src/App.js
+++ src/App.js
@@ -1,91 +1,65 @@
-// import React, { useEffect, useState } from "react";
-// import axios from "axios";
-// import "./App.css";
-
-// function App() {
-//   const [receiveData, setreceiveData] = useState([]);
-//   // const [state, setstate] = useState(initialState)
-//   // const [frog, setfrog] = useState(false)
-//   const getData = () => {
-//     axios
-//       .get("http://localhost:8080")
-//       .then(res => res.data)
-//       .then(res => setreceiveData(res))
-//   };
-// //   axios.post('/songs', "http://localhost:8080",{
-// //     title: 'Fred',
-// //     singer: 'Flintstone',
-// //     listen:true
-// //   })
-// //   .then(res => res.data)
-// //   .then(res => setreceiveData(res))
-// // };
-//   useEffect(() => {
-//     getData()
-//     // console.log("I changed because of frog")
-//   },[]);
-//   const deleteData=(id)=>{
-//     console.log(id)
-//     axios.delete(`http://localhost:8080/${id}`)
-//   }
-  
-
-//   return (
-//     <div>
-
-//     {/* <button onClick={()=>setfrog(!frog)}>Refresh</button> */}
-//     <div className="App">
-//       {receiveData.map(onedata => (
-//         <div key={onedata._id}>
-//           <h1>{onedata.title}</h1>
-//           <h1>{onedata.singer}</h1>
-//           <h1>{onedata.name}</h1>
-//           <h1>{onedata.listen}</h1>
-//           <button onClick={()=>deleteData(onedata._id)}>delete</button>
-//         </div>
-//       ))}
-//     </div>
-//       </div>
-//   );
-// }
-
-// export default App;
-
-
-
-import React ,{useState}from 'react'
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function App() {
-  const [input, setInput] = useState('')
-  const [input1, setInput1] = useState('')
-  const onFormSubmit=(event)=>{
-    event.preventDefault()
+  const [data, setdata] = useState([]);
+  const [input, setInput] = useState("");
+  const [input1, setInput1] = useState("");
+  const onFormSubmit = event => {
+    event.preventDefault();
     console.log(input);
-    axios.post("http://localhost:8080",{
-    name:input,
-    listen:false,
-    singer:input1
-  }
-    )
-  }
-  
+    axios.post("http://localhost:8080", {
+      name: input,
+      listen: false,
+      singer: input1
+    });
+  };
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080")
+      // .then(res=>console.log(res.data))
+      .then(res => setdata(res.data));
+    console.log(data);
+
+    // console.log('hiii')
+  }, []);
+  const deleteData = id => {
+    // console.log("id deleted")
+    console.log(id);
+    axios.delete(`http://localhost:8080/${id}`);
+  };
+  const editData = id => {
+    // console.log("id deleted")
+    // console.log(id);
+    var name=prompt(`edit this name`)
+    var singer=prompt(`edit this singer`)
+    axios.put(
+      `http://localhost:8080/${id}`,
+      {
+        title: "vjdj",
+        singer: singer,
+        name: name
+      }
+      // .then(res => console.log(res))
+    );
+  };
   return (
-    <form onSubmit={(e)=>onFormSubmit(e)}>
-      <input type="text" onChange={(event) => setInput(event.target.value)}/>
-      <input type="text" onChange={(event) => setInput1(event.target.value)
-        
-      }/>
-      <input type="submit" value="submit"/>
-      
+    <div>
+      <form onSubmit={e => onFormSubmit(e)}>
+        <input type="text" onChange={event => setInput(event.target.value)} />
+        <input type="text" onChange={event => setInput1(event.target.value)} />
+        <input type="submit" value="submit" />
       </form>
-
-  )
+      {data.map(item => (
+        <div key={item._id}>
+          <h1>
+            title:-{item.title} , singer:-{item.singer},name:-{item.name}
+          </h1>
+          {/* <h1>{item.singer}</h1> */}
+          <button onClick={() => deleteData(item._id)}>delete</button>
+          <button onClick={() => editData(item._id)}>edit</button>
+        </div>
+      ))}
+    </div>
+  );
 }
-
-
-
-
-
-
-
